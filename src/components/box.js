500cae0fef1ec11ebb31d6454fb543ac5543fdab
Salem <mymainemail0501@gmail.com>
2019-10-15 13:54:49 -0700

    cards

--- src/components/box.js
+++ src/components/box.js
@@ -5,43 +5,78 @@ import Hydroveggie from '../images/stock-hydroveggie.jpg'
 import Nightlight from '../images/stock-nightlight.jpg'
 import UVsterilize from '../images/stock-uvsterilize.jpg'
 
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, Button, CardHeader, CardDeck
+} from 'reactstrap';
+
 
 const Box = () => (
 
         <div id="main">
+			<CardDeck>
 			<div class="inner">
 
 				<div class="thumbnails">
-
+					
 					<div class="box">
-							<img src={Nightlight} alt="" class="image fit"/>
+					
+					<Card>
+					<CardImg top width="100%" src={Nightlight} class="img fit" alt="Card image cap" />
+					<CardHeader tag="h3">Header</CardHeader>
+        				<CardBody>
 								<div class="inner">
+									
+								<CardTitle>
 									<h3>Nascetur nunc varius commodo</h3>
-									<p>Interdum amet accumsan placerat commodo ut amet aliquam blandit nunc tempor lobortis nunc non. Mi accumsan.</p>
-									<a href="https://youtu.be/s6zR2T9vn2c" class="button fit" data-poptrox="youtube,800x400">Watch</a>
+								</CardTitle>
+									<CardText tag="p">	
+										Interdum amet accumsan placerat commodo ut amet aliquam blandit nunc tempor lobortis nunc non. Mi accumsan.
+									</CardText>
+									<Button color="warning">Go somewhere</Button>	
 								</div>
+						</CardBody>
+						</Card>
+					</div>
+				
+					<div class="box">
+					<Card>
+					<CardImg top width="100%" src={Hydroveggie} class="img fit" alt="Card image cap" />
+					<CardHeader tag="h3">Header</CardHeader>
+        				<CardBody>
+								<div class="inner">
+									
+								<CardTitle>
+									<h3>Nascetur nunc varius commodo</h3></CardTitle>
+								<CardText tag="p">Interdum amet accumsan placerat commodo ut amet aliquam blandit nunc tempor lobortis nunc non. Mi accumsan.
+								</CardText>
+								<Button color="warning">Go somewhere</Button>
+								</div>
+						</CardBody>
+					</Card>					
 					</div>
 
 					<div class="box">
-							<img src={Hydroveggie} alt="" class="image fit"/>
+					<Card>
+					<CardImg top width="100%" src={UVsterilize} class="img fit" alt="Card image cap" />
+					<CardHeader tag="h3">Header</CardHeader>
+        				<CardBody>
 								<div class="inner">
+								<CardTitle>
 									<h3>Nascetur nunc varius commodo</h3>
-									<p>Interdum amet accumsan placerat commodo ut amet aliquam blandit nunc tempor lobortis nunc non. Mi accumsan.</p>
-									<a href="https://youtu.be/s6zR2T9vn2c" class="button style2 fit" data-poptrox="youtube,800x400">Watch</a>
+								</CardTitle>	
+								<CardText tag="p">Interdum amet accumsan placerat commodo ut amet aliquam blandit nunc tempor lobortis nunc non. Mi accumsan.
+								</CardText>
+								<Button color="warning">Go somewhere</Button>
 								</div>
+						</CardBody>
+					</Card>	
 					</div>
-
-					<div class="box">
-							<img src={UVsterilize} alt="" class="image fit"/>
-								<div class="inner">
-									<h3>Nascetur nunc varius commodo</h3>
-									<p>Interdum amet accumsan placerat commodo ut amet aliquam blandit nunc tempor lobortis nunc non. Mi accumsan.</p>
-									<a href="https://youtu.be/s6zR2T9vn2c" class="button style2 fit" data-poptrox="youtube,800x400">Watch</a>
-								</div>
-					</div>
+					
 				</div>
 
 			</div>
+		</CardDeck>
 		</div>
 )
 
