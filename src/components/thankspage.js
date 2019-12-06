450497547643f607724a17a3c21c45b01010545d
salem <salem@laCosa>
2019-12-06 05:39:24 -0800

    fuck git

---
+++ src/components/thankspage.js
@@ -0,0 +1,17 @@
+import React from "react"
+import '../css/thanks.css'
+import '../css/main.css'
+
+const Thankspage = () => (
+    <section className="section-thanks">
+        <div className="row">
+        
+            <div className="section-thanks thanks container col-md-12">
+                <h1>Thank you!</h1>
+                <p>We will respond to you shortly as necessary.</p>
+            </div>
+        </div>
+    </section>
+)
+
+export default Thankspage
---
+++ src/pages/thanks.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Layout from "../components/layout"
+import Thankspage from "../components/thankspage"
+import { Container } from 'reactstrap'
+import '../components/layout.css'
+
+const Thanks = () => (
+    <div>
+        <Layout>
+            <Container>
+                <Thankspage />
+                
+            </Container>
+        </Layout>
+    </div>
+)
+
+export default Thanks
